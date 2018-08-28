c5a4dd78bee77dddb51eaead8e3af320afb5953e
SoggyPiggy <soggypiggy@gmail.com>
2018-08-28 00:51:01 -0400

    Update BrowserWindow to have min width and height

--- src/background.js
+++ src/background.js
@@ -21,7 +21,7 @@ let mainWindow;
 // Standard scheme must be registered before the app is ready
 protocol.registerStandardSchemes(['app'], { secure: true });
 function createMainWindow() {
-	const window = new BrowserWindow();
+	const window = new BrowserWindow({ minWidth: 480, minHeight: 400 });
 
 	if (isDevelopment) {
 		// Load the url of the dev server if in development mode
