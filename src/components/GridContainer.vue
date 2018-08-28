bc43b2bca027af2a8aadc86db295844ca7cda0d1
SoggyPiggy <soggypiggy@gmail.com>
2018-08-28 00:51:37 -0400

    Add GridContainer for custom css masonary

---
+++ src/components/GridContainer.vue
@@ -0,0 +1,37 @@
+<style>
+.grid-container {
+	display: grid;
+}
+</style>
+
+<template>
+	<div class="grid-container" 
+	:style="`
+	grid-gap: ${gutter}px;
+	margin: ${margin};
+	grid-template-columns: repeat(auto-fit, minmax(${width}px, 1fr));
+	grid-template-rows: repeat(auto-fit);
+	`">
+		<slot/>
+	</div>
+</template>
+
+<script>
+export default {
+	props: {
+		width: {
+			type: Number,
+			required: true,
+		},
+		gutter: {
+			type: Number,
+			default: 8,
+		},
+		margin: {
+			type: String,
+			default: '8px',
+		},
+	}
+}
+</script>
+
