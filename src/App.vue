e810821a580a3d65a5b475291b99712dfcb0c0c1
SoggyPiggy <soggypiggy@gmail.com>
2018-08-30 02:10:15 -0400

    Add .scrollview class to App Added a class to hide the scrollbar

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,22 @@ html, body {
   width: 100vw;
   height: 100vh;
 }
+
+.scrollview {
+	width: 100%;
+	height: 100%;
+	overflow: hidden;
+	position: relative;
+}
+
+.scrollview > * {
+	position: absolute;
+	left: 0;
+	top: 0;
+	right: -17px;
+	bottom: 0;
+	overflow-y: scroll;
+}
 </style>
 
 <template>
