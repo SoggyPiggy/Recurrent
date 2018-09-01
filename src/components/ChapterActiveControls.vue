d37fe05032c6bb74b382b3860df78c689edf9388
SoggyPiggy <soggypiggy@gmail.com>
2018-09-01 16:43:37 -0400

    Add ChapterActiveControls

---
+++ src/components/ChapterActiveControls.vue
@@ -0,0 +1,37 @@
+<template>
+	<a-button-group>
+		<a-button @click="game.chapter.activate()" :disabled="!canPlay">
+			<a-icon type="caret-right"/>
+		</a-button>
+		<a-button @click="game.chapter.deactivate()" :disabled="!canPause">
+			<a-icon type="pause"/>
+		</a-button>
+	</a-button-group>
+</template>
+
+<script>
+import { Game } from 'recurrent-core';
+
+const game = Game.instance;
+
+export default {
+	data() {
+		return {
+			game,
+		};
+	},
+	computed: {
+		canPlay() {
+			if (!game.chapter) return false;
+			if (game.chapter.active) return false;
+			if (game.player.status.dead) return false;
+			return true;
+		},
+		canPause() {
+			if (!game.chapter) return false;
+			if (!game.chapter.active) return false;
+			return true;
+		},
+	},
+};
+</script>
