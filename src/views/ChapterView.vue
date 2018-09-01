f556adbf7b9ca3ada0cc66da66b1caddd60fc430
SoggyPiggy <soggypiggy@gmail.com>
2018-09-01 17:08:54 -0400

    Add base ChapterView

---
+++ src/views/ChapterView.vue
@@ -0,0 +1,39 @@
+<template>
+	<a-tabs defaultActiveKey="general">
+		<a-tab-pane tab="General" key="general">
+		</a-tab-pane>
+		<a-tab-pane key="favorites">
+			<span slot="tab">
+				<a-icon type="star"/>
+				Favorites
+			</span>
+		</a-tab-pane>
+		<a-tab-pane tab="Player" key="player">
+		</a-tab-pane>
+		<a-tab-pane tab="Quest" key="quest">
+		</a-tab-pane>
+		<a-tab-pane tab="Objective" key="objective">
+		</a-tab-pane>
+	</a-tabs>
+</template>
+
+<script>
+import { Game } from 'recurrent-core';
+
+const game = Game.instance;
+
+export default {
+	props: {
+		chapter: {
+			type: Object,
+			default: () => game.chapter,
+		},
+	},
+	data() {
+		return {
+			game,
+		};
+	},
+};
+</script>
+
