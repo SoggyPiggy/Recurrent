6df247be3067dd70bd8d3b12764d3d3782d934ec
SoggyPiggy <soggypiggy@gmail.com>
2018-09-01 18:16:33 -0400

    Update moved content to seperate component in NewView

---
+++ src/components/NewView/CardCharacterCreation.vue
@@ -0,0 +1,57 @@
+<template>
+	<a-card>
+		<a-tooltip
+		:title="canNew"
+		placement="bottom"
+		:trigger="canNew !== true?'hover':'null'">
+			<a-button
+			type="primary"
+			:disabled="canNew !== true"
+			@click="game.chapters.newChapter()">
+				New Character
+			</a-button>
+		</a-tooltip>
+		<a-tooltip
+		:title="canReroll"
+		placement="bottom"
+		:trigger="canReroll !== true?'hover':'null'">
+			<a-button
+			:disabled="canReroll !== true"
+			@click="game.player.roll()">
+				Reroll Attributes
+			</a-button>
+		</a-tooltip>
+		<chapter-active-controls/>
+	</a-card>
+</template>
+
+<script>
+import { Game } from 'recurrent-core';
+import ChapterActiveControls from '@/components/ChapterActiveControls.vue';
+
+const game = Game.instance;
+
+export default {
+	data() {
+		return {
+			game,
+		};
+	},
+	components: {
+		ChapterActiveControls,
+	},
+	computed: {
+		canNew() {
+			if (!game.chapter) return true;
+			if (game.chapter.active) return 'Pause your current character before making a new.';
+			if (game.chapter.ticks <= 0) return 'You already have a new character.';
+			return true;
+		},
+		canReroll() {
+			if (!game.chapter) return 'You have to create a character first.';
+			if (game.chapter.ticks > 0) return 'You can only reroll a new character.';
+			return true;
+		},
+	},
+};
+</script>
