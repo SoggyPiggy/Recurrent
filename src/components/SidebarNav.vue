92290d292d33d9952983473a39bdb5ce656735b6
SoggyPiggy <soggypiggy@gmail.com>
2018-09-01 17:08:11 -0400

    Add functionality to SidebarNave

--- src/components/SidebarNav.vue
+++ src/components/SidebarNav.vue
@@ -1,22 +1,39 @@
 <template>
 	<a-layout-sider collapsible>
-		<a-menu theme="dark">
-			<a-menu-item>
+		<a-menu theme="dark" @select="({ key }) => this.$emit('select', key)" :defaultSelectedKeys="['home']">
+			<a-menu-item key="home">
 				<a-icon type="home"/>
 				<span>Home</span>
 			</a-menu-item>
-			<a-menu-item>
+			<a-menu-item key="active" :disabled="!(game.chapters.items.length > 0)">
 				<a-icon type="user"/>
 				<span>Active Character</span>
 			</a-menu-item>
-			<a-menu-item>
-				<a-icon type="team"/>
-				<span>Past Characters</span>
+			<a-menu-item key="past" :disabled="(!(game.chapters.items.length > 1)) || true">
+				<a-popover content="Coming soon..." placement="right">
+					<a-icon type="team"/>
+					<span>Past Characters</span>
+				</a-popover>
 			</a-menu-item>
-			<a-menu-item>
-				<a-icon type="setting"/>
-				<span>Settings</span>
+			<a-menu-item key="new">
+				<a-icon type="user-add"/>
+				<span>New Character</span>
 			</a-menu-item>
 		</a-menu>
 	</a-layout-sider>
 </template>
+
+<script>
+import { Game } from 'recurrent-core';
+
+const game = Game.instance;
+
+export default {
+	data() {
+		return {
+			game,
+		};
+	},
+};
+</script>
+
