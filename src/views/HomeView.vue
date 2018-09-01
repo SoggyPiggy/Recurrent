679eadb2ab22f0b0b2bfa6c3f15ea9e20db8b1db
SoggyPiggy <soggypiggy@gmail.com>
2018-09-01 17:08:36 -0400

    Add base HomeView

---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<template>
+	<a-tabs defaultActiveKey="home">
+		<a-tab-pane tab="Home" key="home">
+		</a-tab-pane>
+		<a-tab-pane tab="Change Log" key="changes">
+		</a-tab-pane>
+		<a-tab-pane tab="Settings" key="settings">
+		</a-tab-pane>
+	</a-tabs>
+</template>
