92e5f46f9cfc9249160ba35ef30d1e04863f8704
SoggyPiggy <soggypiggy@gmail.com>
2018-09-02 03:41:51 -0400

    Add newview class to NewView

--- src/views/NewView.vue
+++ src/views/NewView.vue
@@ -1,5 +1,19 @@
+<style>
+.newview {
+	display: flex;
+	flex-direction: column;
+}
+
+.newview > * {
+	margin-top: 16px;
+}
+.newview > *:first-child {
+	margin-top: 0;
+}
+</style>
+
 <template>
-	<div>
+	<div class="newview">
 		<card-character-creation/>
 	</div>
 </template>
