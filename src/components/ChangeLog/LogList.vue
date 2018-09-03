31150ae240ef4bef6e60bf9f602c2e05de3cc841
SoggyPiggy <soggypiggy@gmail.com>
2018-09-02 22:46:10 -0400

    Add LogList

---
+++ src/components/ChangeLog/LogList.vue
@@ -0,0 +1,20 @@
+<template>
+	<a-list itemLayout="horizonal" :dataSource="list">
+		<a-list-item slot="renderItem" slot-scope="item, index">
+			<a-list-item-meta>
+				<span slot="title">{{item}}</span>
+			</a-list-item-meta>
+		</a-list-item>
+	</a-list>
+</template>
+
+<script>
+export default {
+	props: {
+		list: {
+			type: Array,
+			required: true,
+		},
+	},
+};
+</script>
