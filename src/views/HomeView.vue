d7b888ee2f60656f2aafc39f30c5742c8a9eaf66
SoggyPiggy <soggypiggy@gmail.com>
2018-09-02 22:47:03 -0400

    Add ChangeLog component to HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,8 +3,19 @@
 		<a-tab-pane tab="Home" key="home">
 		</a-tab-pane>
 		<a-tab-pane tab="Change Log" key="changes">
+			<change-log/>
 		</a-tab-pane>
 		<a-tab-pane tab="Settings" key="settings">
 		</a-tab-pane>
 	</a-tabs>
 </template>
+
+<script>
+import ChangeLog from '@/components/ChangeLog.vue';
+
+export default {
+	components: {
+		ChangeLog,
+	},
+};
+</script>
