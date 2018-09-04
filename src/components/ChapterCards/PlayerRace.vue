08d8b4a23d347bda0fcd63670b1a486520045a9c
SoggyPiggy <soggypiggy@gmail.com>
2018-09-04 07:22:09 -0400

    Add PlayerRace chapter module

---
+++ src/components/ChapterCards/PlayerRace.vue
@@ -0,0 +1,30 @@
+<template>
+	<a-card title="Character Race" style="margin:4px;grid-row-end:span 137;">
+		<favorite-button slot="extra" component="player-race"/>
+		<div class="text-ellipsis">{{chapter.player.race}}</div>
+	</a-card>
+</template>
+
+<script>
+import FavoriteButton from '@/components/FavoriteButton.vue';
+
+export default {
+	name: 'player-race',
+	props: {
+		chapter: {
+			type: Object,
+			required: true,
+		},
+	},
+	computed: {
+		getStatus() {
+			if (this.chapter.player.status.dead) return 'exception';
+			if (this.chapter.active) return 'active';
+			return null;
+		},
+	},
+	components: {
+		FavoriteButton,
+	},
+};
+</script>
