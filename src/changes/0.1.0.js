7049164b938096b94ac38975ad958911e2937326
SoggyPiggy <soggypiggy@gmail.com>
2018-09-04 08:24:44 -0400

    Update ChangeLog Add difference between core game and app changelogs

--- src/changes/0.1.0.js
+++ src/changes/0.1.0.js
@@ -1,11 +1,29 @@
 export default {
 	title: 'Initial Release',
 	version: '0.1.0',
-	log: {
+	core: {
 		additions: [],
 		changes: [],
 		fixes: [],
-		other: [
+		notes: [],
+	},
+	app: {
+		additions: [
+			'Chapter Module: Activity Controls',
+			'Chapter Module: General Progress',
+			'Chapter Module: Objective Information',
+			'Chapter Module: Player Race',
+			'Chapter Module: Player Name',
+			'Chapter Module: Player Attributes',
+			'Chapter Module: Objective Progress',
+			'Chapter Module: Quest Progress',
+			'Character Creation Module: New Character',
+			'Character Creation Module: Attributes',
+			'Character Creation Module: Information',
+		],
+		changes: [],
+		fixes: [],
+		notes: [
 			'Initail Release',
 		],
 	},
--- src/components/ChangeLog.vue
+++ src/components/ChangeLog.vue
@@ -1,25 +1,64 @@
+<style>
+.changelog {
+	display: flex;
+	flex-direction: column;
+}
+
+.changelog > * {
+	margin-bottom: 16px;
+}
+.changelog > *:last-child {
+	margin-bottom: 0;
+}
+
+.changelog .ant-collapse .ant-collapse-content-box,
+.changelog .ant-collapse .ant-list-item {
+	padding: 4px 0;
+}
+</style>
+
 <template>
-	<a-card :title="selectedLog.title">
-		<a-select style="width: 200px;" slot="extra" v-model="selectedIndex">
-			<a-select-option v-for="(log, index) of changelogs" :key="index">
-				{{log.version}}
-			</a-select-option>
-		</a-select>
-		<a-collapse accordion>
-			<a-collapse-panel v-if="additions.length > 0" header="New" key="new">
-				<log-list :list="additions"/>
-			</a-collapse-panel>
-			<a-collapse-panel v-if="changes.length > 0" header="Changes" key="changes">
-				<log-list :list="changes"/>
-			</a-collapse-panel>
-			<a-collapse-panel v-if="fixes.length > 0" header="Fixes" key="fixes">
-				<log-list :list="fixes"/>
-			</a-collapse-panel>
-			<a-collapse-panel v-if="other.length > 0" header="Notes" key="other">
-				<log-list :list="other"/>
-			</a-collapse-panel>
-		</a-collapse>
-	</a-card>
+	<div class="changelog">
+		<a-card>
+				<a-select v-model="selectedIndex" style="width:100%;">
+					<a-select-option v-for="(log, index) of changelogs" :key="index">
+						v{{log.version}} - {{log.title}}
+					</a-select-option>
+				</a-select>
+		</a-card>
+		<a-card v-if="showApp" title="App Change Log">
+			<a-collapse accordion>
+				<a-collapse-panel v-if="showAppAdditions" header="New" key="new">
+					<log-list :list="appAdditions"/>
+				</a-collapse-panel>
+				<a-collapse-panel v-if="showAppChanges" header="Changes" key="changes">
+					<log-list :list="appChanges"/>
+				</a-collapse-panel>
+				<a-collapse-panel v-if="showAppFixes" header="Fixes" key="fixes">
+					<log-list :list="appFixes"/>
+				</a-collapse-panel>
+				<a-collapse-panel v-if="showAppNotes" header="Notes" key="notes">
+					<log-list :list="appNotes"/>
+				</a-collapse-panel>
+			</a-collapse>
+		</a-card>
+		<a-card v-if="showCore" title="Core Game Change Log">
+			<a-collapse accordion>
+				<a-collapse-panel v-if="showCoreAdditions" header="New" key="new">
+					<log-list :list="coreAdditions"/>
+				</a-collapse-panel>
+				<a-collapse-panel v-if="showCoreChanges" header="Changes" key="changes">
+					<log-list :list="coreChanges"/>
+				</a-collapse-panel>
+				<a-collapse-panel v-if="showCoreFixes" header="Fixes" key="fixes">
+					<log-list :list="coreFixes"/>
+				</a-collapse-panel>
+				<a-collapse-panel v-if="showCoreNotes" header="Notes" key="notes">
+					<log-list :list="coreNotes"/>
+				</a-collapse-panel>
+			</a-collapse>
+		</a-card>
+	</div>
 </template>
 
 <script>
@@ -40,18 +79,37 @@ export default {
 		selectedLog() {
 			return this.changelogs[this.selectedIndex];
 		},
-		additions() {
-			return this.selectedLog.log.additions;
+		showCore() {
+			if (this.selectedLog.core.additions.length > 0) return true;
+			if (this.selectedLog.core.changes.length > 0) return true;
+			if (this.selectedLog.core.fixes.length > 0) return true;
+			if (this.selectedLog.core.notes.length > 0) return true;
+			return false;
 		},
-		changes() {
-			return this.selectedLog.log.changes;
+		showCoreAdditions() { return this.selectedLog.core.additions.length > 0 },
+		coreAdditions() { return this.selectedLog.core.additions },
+		showCoreChanges() { return this.selectedLog.core.changes.length > 0 },
+		coreChanges() { return this.selectedLog.core.changes },
+		showCoreFixes() { return this.selectedLog.core.fixes.length > 0 },
+		coreFixes() { return this.selectedLog.core.fixes },
+		showCoreNotes() { return this.selectedLog.core.notes.length > 0 },
+		coreNotes() { return this.selectedLog.core.notes },
+
+		showApp() {
+			if (this.selectedLog.app.additions.length > 0) return true;
+			if (this.selectedLog.app.changes.length > 0) return true;
+			if (this.selectedLog.app.fixes.length > 0) return true;
+			if (this.selectedLog.app.notes.length > 0) return true;
+			return false;
 		},
-		fixes() {
-			return this.selectedLog.log.fixes;
-		},
-		other() {
-			return this.selectedLog.log.other;
-		},
+		showAppAdditions() { return this.selectedLog.app.additions.length > 0 },
+		appAdditions() { return this.selectedLog.app.additions },
+		showAppChanges() { return this.selectedLog.app.changes.length > 0 },
+		appChanges() { return this.selectedLog.app.changes },
+		showAppFixes() { return this.selectedLog.app.fixes.length > 0 },
+		appFixes() { return this.selectedLog.app.fixes },
+		showAppNotes() { return this.selectedLog.app.notes.length > 0 },
+		appNotes() { return this.selectedLog.app.notes },
 	},
 };
 </script>
