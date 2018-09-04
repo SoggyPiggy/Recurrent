c468acc88b00d07b106fea4c84058825edfcdca9
SoggyPiggy <soggypiggy@gmail.com>
2018-09-04 07:21:51 -0400

    Add PlayerName chapter module

---
+++ src/components/ChapterCards/PlayerName.vue
@@ -0,0 +1,31 @@
+<template>
+	<a-card title="Character Name" style="margin:4px;grid-row-end:span 137;">
+		<favorite-button slot="extra" component="player-name"/>
+		<div class="text-ellipsis">{{chapter.player.name}}</div>
+	</a-card>
+</template>
+
+<script>
+import FavoriteButton from '@/components/FavoriteButton.vue';
+
+export default {
+	name: 'player-name',
+	general: true,
+	props: {
+		chapter: {
+			type: Object,
+			required: true,
+		},
+	},
+	computed: {
+		getStatus() {
+			if (this.chapter.player.status.dead) return 'exception';
+			if (this.chapter.active) return 'active';
+			return null;
+		},
+	},
+	components: {
+		FavoriteButton,
+	},
+};
+</script>
