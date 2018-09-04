a5653eb51aecc82dd66a8f6895ee71705d2d702b
SoggyPiggy <soggypiggy@gmail.com>
2018-09-04 07:24:32 -0400

    Add chapter modules to ChapterView

--- src/views/ChapterView.vue
+++ src/views/ChapterView.vue
@@ -1,6 +1,9 @@
 <template>
 	<a-tabs defaultActiveKey="general">
 		<a-tab-pane tab="General" key="general">
+			<grid-container :width="258" :height="1" :gutter="0" margin="0" justify="center">
+				<component v-for="item of generalComponents" :key="item" :chapter="chapter" :is="item"/>
+			</grid-container>
 		</a-tab-pane>
 		<a-tab-pane key="favorites" disabled>
 			<span slot="tab">
@@ -9,16 +12,35 @@
 			</span>
 		</a-tab-pane>
 		<a-tab-pane tab="Player" key="player">
+			<grid-container :width="258" :height="1" :gutter="0" margin="0" justify="center">
+				<component v-for="item of playerComponents" :key="item" :chapter="chapter" :is="item"/>
+			</grid-container>
 		</a-tab-pane>
 		<a-tab-pane tab="Quest" key="quest">
+			<grid-container :width="258" :height="1" :gutter="0" margin="0" justify="center">
+				<component v-for="item of questComponents" :key="item" :chapter="chapter" :is="item"/>
+			</grid-container>
 		</a-tab-pane>
 		<a-tab-pane tab="Objective" key="objective">
+			<grid-container :width="258" :height="1" :gutter="0" margin="0" justify="center">
+				<component v-for="item of objectiveComponents" :key="item" :chapter="chapter" :is="item"/>
+			</grid-container>
 		</a-tab-pane>
 	</a-tabs>
 </template>
 
 <script>
 import { Game } from 'recurrent-core';
+import GridContainer from '@/components/GridContainer.vue';
+
+import GeneralProgress from '@/components/ChapterCards/GeneralProgress.vue';
+import GeneralActiveControls from '@/components/ChapterCards/GeneralActiveControls.vue';
+import QuestProgress from '@/components/ChapterCards/QuestProgress.vue';
+import ObjectiveProgress from '@/components/ChapterCards/ObjectiveProgress.vue';
+import ObjectiveInformation from '@/components/ChapterCards/ObjectiveInformation.vue';
+import PlayerName from '@/components/ChapterCards/PlayerName.vue';
+import PlayerRace from '@/components/ChapterCards/PlayerRace.vue';
+import PlayerAttributes from '@/components/ChapterCards/PlayerAttributes.vue';
 
 const game = Game.instance;
 
@@ -30,9 +52,43 @@ export default {
 		},
 	},
 	data() {
+		const generalComponents = [
+			'player-name',
+			'player-attributes',
+			'general-active-controls',
+			'general-progress',
+			'objective-information',
+		];
+		const playerComponents = [
+			'player-name',
+			'player-race',
+			'player-attributes',
+		];
+		const questComponents = [
+			'quest-progress',
+		];
+		const objectiveComponents = [
+			'objective-progress',
+			'objective-information',
+		];
 		return {
 			game,
+			generalComponents,
+			playerComponents,
+			questComponents,
+			objectiveComponents,
 		};
+	},
+	components: {
+		GridContainer,
+		GeneralProgress,
+		GeneralActiveControls,
+		QuestProgress,
+		ObjectiveProgress,
+		ObjectiveInformation,
+		PlayerName,
+		PlayerRace,
+		PlayerAttributes,
 	},
 };
 </script>
