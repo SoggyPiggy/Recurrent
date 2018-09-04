b341af2fbfa0b00ace574014709c27ec1e8ea2a5
SoggyPiggy <soggypiggy@gmail.com>
2018-09-04 07:20:37 -0400

    Add ObjectiveInformation chapter module

---
+++ src/components/ChapterCards/ObjectiveInformation.vue
@@ -0,0 +1,32 @@
+<template>
+	<a-card title="Objective Information" style="margin:4px;grid-row-end:span 172;">
+		<favorite-button slot="extra" component="objective-information"/>
+		<a-popover :title="chapter.objective.title" :content="chapter.objective.description">
+			<p>{{chapter.objective.title}}</p>
+		</a-popover>
+	</a-card>
+</template>
+
+<script>
+import FavoriteButton from '@/components/FavoriteButton.vue';
+
+export default {
+	name: 'objective-information',
+	props: {
+		chapter: {
+			type: Object,
+			required: true,
+		},
+	},
+	computed: {
+		getStatus() {
+			if (this.chapter.player.status.dead) return 'exception';
+			if (this.chapter.active) return 'active';
+			return null;
+		},
+	},
+	components: {
+		FavoriteButton,
+	},
+};
+</script>
