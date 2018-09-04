39a5e688c33388f4ab1f6956bcb24c5d75aa4c8e
SoggyPiggy <soggypiggy@gmail.com>
2018-09-04 01:27:04 -0400

    Add FavoriteButton

---
+++ src/components/FavoriteButton.vue
@@ -0,0 +1,30 @@
+<template>
+	<a-button
+	shape="circle"
+	size="small"
+	:icon="isFavorite ? 'star' : 'star-o'"
+	@click="toggleFavorite"
+	/>
+</template>
+
+<script>
+export default {
+	props: {
+		component: {
+			type: String,
+			required: true,
+		},
+	},
+	computed: {
+		isFavorite() {
+			return false;
+		},
+	},
+	methods: {
+		toggleFavorite() {
+			this.$emit('favorite');
+		},
+	},
+};
+</script>
+
