9aaa57b5e89403e7da858d3ac6f456334995403d
SoggyPiggy <soggypiggy@gmail.com>
2018-09-09 18:01:35 -0400

    Add version to HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <template>
 	<a-tabs defaultActiveKey="home">
 		<a-tab-pane tab="Landing" key="home">
-			<h1>Get started by heading to New Character from the Menu<br/>
-			<a-icon type="left-square"/></h1>
-			<p></p>
+			<h1>
+				<a-icon type="left-square"/>
+				Get started by heading to New Character from the Menu
+			</h1>
+			<p>Version: {{version}}</p>
 		</a-tab-pane>
 		<a-tab-pane tab="Change Log" key="changes">
 			<change-log/>
@@ -14,9 +16,15 @@
 </template>
 
 <script>
+import { version } from '@/../package';
 import ChangeLog from '@/components/ChangeLog.vue';
 
 export default {
+	data() {
+		return {
+			version,
+		};
+	},
 	components: {
 		ChangeLog,
 	},
