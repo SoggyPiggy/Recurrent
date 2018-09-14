a07c824dc89ec3ed9e40e553472f74f7b5e0ffab
SoggyPiggy <soggypiggy@gmail.com>
2018-09-14 14:16:30 -0400

    Update settings tab on HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,10 +7,10 @@
 			</h1>
 			<p>Version: {{version}}</p>
 		</a-tab-pane>
+		<a-tab-pane tab="Settings" key="settings" disabled>
+		</a-tab-pane>
 		<a-tab-pane tab="Change Log" key="changes">
 			<change-log/>
-		</a-tab-pane>
-		<a-tab-pane tab="Settings" key="settings" disabled>
 		</a-tab-pane>
 		<a-tab-pane tab="Feedback" key="feedback">
 			<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSf9O4mf1oBdLPHnNz6-ykB-oaH_iQO-YIm2B0ecuaCLnwmNXA/viewform?embedded=true" width="100%" height="844" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
