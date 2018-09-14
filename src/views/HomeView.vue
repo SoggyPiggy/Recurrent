2693bc1e36a70e340eadb8255c72d4fdb44b5594
SoggyPiggy <soggypiggy@gmail.com>
2018-09-14 14:18:15 -0400

    Add about tab on HomeView about tab is disabled right now

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -15,6 +15,8 @@
 		<a-tab-pane tab="Feedback" key="feedback">
 			<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSf9O4mf1oBdLPHnNz6-ykB-oaH_iQO-YIm2B0ecuaCLnwmNXA/viewform?embedded=true" width="100%" height="844" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
 		</a-tab-pane>
+		<a-tab-pane tab="About" key="about" disabled>
+		</a-tab-pane>
 	</a-tabs>
 </template>
 
