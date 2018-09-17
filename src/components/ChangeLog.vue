6ea3ff0dc29002ce13628ca5587aeb468d70bc26
SoggyPiggy <soggypiggy@gmail.com>
2018-09-17 04:02:29 -0400

    Update ChangeLog Too tired to make message. Change behaviour, property names, and loading

--- src/components/ChangeLog.vue
+++ src/components/ChangeLog.vue
@@ -20,41 +20,41 @@
 <template>
 	<div class="changelog">
 		<a-card>
-				<a-select v-model="selectedIndex" style="width:100%;">
+				<a-select v-model="logIndex" style="width:100%;">
 					<a-select-option v-for="(log, index) of changelogs" :key="index">
 						v{{log.version}} - {{log.title}}
 					</a-select-option>
 				</a-select>
 		</a-card>
 		<a-card v-if="showApp" title="App Change Log">
-			<a-collapse accordion>
-				<a-collapse-panel v-if="showAppAdditions" header="New" key="new">
-					<log-list :list="appAdditions"/>
+			<a-collapse v-model="activeKey">
+				<a-collapse-panel v-if="showAppAdditions" header="New" :key="`aNew${logIndex}`">
+					<log-list :list="selectedLog.app.additions"/>
 				</a-collapse-panel>
-				<a-collapse-panel v-if="showAppChanges" header="Changes" key="changes">
-					<log-list :list="appChanges"/>
+				<a-collapse-panel v-if="showAppChanges" header="Changes" :key="`aChanges${logIndex}`">
+					<log-list :list="selectedLog.app.changes"/>
 				</a-collapse-panel>
-				<a-collapse-panel v-if="showAppFixes" header="Fixes" key="fixes">
-					<log-list :list="appFixes"/>
+				<a-collapse-panel v-if="showAppFixes" header="Fixes" :key="`aFixes${logIndex}`">
+					<log-list :list="selectedLog.app.fixes"/>
 				</a-collapse-panel>
-				<a-collapse-panel v-if="showAppNotes" header="Notes" key="notes">
-					<log-list :list="appNotes"/>
+				<a-collapse-panel v-if="showAppNotes" header="Notes" :key="`aNotes${logIndex}`">
+					<log-list :list="selectedLog.app.notes"/>
 				</a-collapse-panel>
 			</a-collapse>
 		</a-card>
 		<a-card v-if="showCore" title="Core Game Change Log">
-			<a-collapse accordion>
-				<a-collapse-panel v-if="showCoreAdditions" header="New" key="new">
-					<log-list :list="coreAdditions"/>
+			<a-collapse v-model="activeKey">
+				<a-collapse-panel v-if="showCoreAdditions" header="New" :key="`cNew${logIndex}`">
+					<log-list :list="selectedLog.core.additions"/>
 				</a-collapse-panel>
-				<a-collapse-panel v-if="showCoreChanges" header="Changes" key="changes">
-					<log-list :list="coreChanges"/>
+				<a-collapse-panel v-if="showCoreChanges" header="Changes" :key="`cChanges${logIndex}`">
+					<log-list :list="selectedLog.core.changes"/>
 				</a-collapse-panel>
-				<a-collapse-panel v-if="showCoreFixes" header="Fixes" key="fixes">
-					<log-list :list="coreFixes"/>
+				<a-collapse-panel v-if="showCoreFixes" header="Fixes" :key="`cFixes${logIndex}`">
+					<log-list :list="selectedLog.core.fixes"/>
 				</a-collapse-panel>
-				<a-collapse-panel v-if="showCoreNotes" header="Notes" key="notes">
-					<log-list :list="coreNotes"/>
+				<a-collapse-panel v-if="showCoreNotes" header="Notes" :key="`cNotes${logIndex}`">
+					<log-list :list="selectedLog.core.notes"/>
 				</a-collapse-panel>
 			</a-collapse>
 		</a-card>
@@ -62,22 +62,37 @@
 </template>
 
 <script>
+import fs from 'fs';
+import compareVersions from 'compare-versions';
 import LogList from '@/components/ChangeLog/LogList.vue';
-import changelogs from '@/changes/';
+
+const changelogs = fs.readdirSync('./src/changes/')
+	// eslint-disable-next-line global-require, import/no-dynamic-require
+	.map(location => (require(`@/changes/${location}`).default))
+	.sort((a, b) => compareVersions(b.version, a.version));
 
 export default {
 	data() {
 		return {
 			changelogs,
-			selectedIndex: 0,
+			activeKeyData: null,
+			logIndex: 0,
 		};
 	},
 	components: {
 		LogList,
 	},
 	computed: {
+		activeKey: {
+			get() {
+				return this.activeKeyData;
+			},
+			set(value) {
+				this.activeKeyData = value[value.length - 1];
+			},
+		},
 		selectedLog() {
-			return this.changelogs[this.selectedIndex];
+			return this.changelogs[this.logIndex];
 		},
 		showCore() {
 			if (this.selectedLog.core.additions.length > 0) return true;
@@ -87,13 +102,9 @@ export default {
 			return false;
 		},
 		showCoreAdditions() { return this.selectedLog.core.additions.length > 0; },
-		coreAdditions() { return this.selectedLog.core.additions; },
 		showCoreChanges() { return this.selectedLog.core.changes.length > 0; },
-		coreChanges() { return this.selectedLog.core.changes; },
 		showCoreFixes() { return this.selectedLog.core.fixes.length > 0; },
-		coreFixes() { return this.selectedLog.core.fixes; },
 		showCoreNotes() { return this.selectedLog.core.notes.length > 0; },
-		coreNotes() { return this.selectedLog.core.notes; },
 
 		showApp() {
 			if (this.selectedLog.app.additions.length > 0) return true;
@@ -103,13 +114,9 @@ export default {
 			return false;
 		},
 		showAppAdditions() { return this.selectedLog.app.additions.length > 0; },
-		appAdditions() { return this.selectedLog.app.additions; },
 		showAppChanges() { return this.selectedLog.app.changes.length > 0; },
-		appChanges() { return this.selectedLog.app.changes; },
 		showAppFixes() { return this.selectedLog.app.fixes.length > 0; },
-		appFixes() { return this.selectedLog.app.fixes; },
 		showAppNotes() { return this.selectedLog.app.notes.length > 0; },
-		appNotes() { return this.selectedLog.app.notes; },
 	},
 };
 </script>
